4872f0f215a98e166eedc0def51b5c4221f99b80
adam-pawelek-cisco <108860785+adam-pawelek-cisco@users.noreply.github.com>
2022-08-10 12:20:44 +0200

    added spring

---
+++ src/main/java/com/example/mojtest/MojtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mojtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mojtest/MojtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mojtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
